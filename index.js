adaef2f07a75f5677af980ef4542fc1d90ed58c1
ecoRise <maheshbodhagire@gmail.com>
2020-01-11 19:07:11 +0530

    package.json update

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = 3000
 
 //joining path of directory 
 const directoryPath = path.join(__dirname, 'assets');
-var images = []
 const app = express();
 app.use(express.static(directoryPath));
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
@@ -27,9 +26,9 @@ app.get('/', (req, res) => {
             // Do whatever you want to do with the file
             var newImageName = "http://localhost:3000/assets/"+ file
             imgArray.push(newImageName)
-            console.log(newImageName);
+            // console.log(newImageName);
         });
-        console.log(imgArray)
+        // console.log(imgArray)
         res.json({data: imgArray})
     });
     
@@ -37,16 +36,3 @@ app.get('/', (req, res) => {
 
 //start server 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-// function getImages(directoryPath, callback) {
-//     var fileType = '.jpg',
-//         files = [], i;
-//     fs.readdir(imageDir, function (err, list) {
-//         for(i=0; i<list.length; i++) {
-//             if(path.extname(list[i]) === fileType) {
-//                 files.push(list[i]); //store the file name into the array files
-//             }
-//         }
-//         callback(err, files);
-//     });
-// }
